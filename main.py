52aa4952612ca5e68be03492d7c27a09e0ad5315
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2021-10-22 16:53:34 +1100

    1st pic, mouse box, cursor

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from ctypes.wintypes import BOOL, HWND, RECT
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+UNIT_SIZE = 50
+COLOR_WHITE = (255, 255, 255)
+
+char_img = pygame.image.load('resource/img/t1.png')
 
 root = tk.Tk()
 root.wm_withdraw() #to hide the main window
@@ -41,13 +45,39 @@ class MyDialog:
 class Char(pygame.sprite.Sprite):
     def  __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill((0, 255, 255))
+        self.image = pygame.transform.scale(char_img, (UNIT_SIZE, UNIT_SIZE))
+        # self.image = pygame.Surface((50, 50))
+        # self.image.fill((0, 255, 255))
         self.rect = self.image.get_rect()
-        self.rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        self.rect.width = UNIT_SIZE
+        self.rect.height = UNIT_SIZE
+        self.rect.x = UNIT_SIZE * ((SCREEN_WIDTH / 2) // UNIT_SIZE)
+        self.rect.y = UNIT_SIZE * ((SCREEN_HEIGHT / 2) // UNIT_SIZE)
 
     def update(self):
         pass
+
+class MouseRec(pygame.sprite.Sprite):
+    def  __init__(self, screen, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.screen = screen
+        # self.image = pygame.Surface((50, 50))
+        # self.image.fill((0, 0, 0))
+        # self.rect = self.image.get_rect()
+        # self.rect.center = (pos[0], pos[1])
+        self.rect = pygame.Rect(pos.x, pos.y, 50, 50)
+
+    def update(self):
+        pygame.draw.rect(self.screen, (0, 0, 0), self.rect)
+
+def draw_mousepos(screen, pos):
+    WIDTH = 50
+    HEIGHT = 50
+    x = UNIT_SIZE * ((pos[0] - WIDTH / 2) // UNIT_SIZE)
+    y = UNIT_SIZE * ((pos[1] - HEIGHT / 2) // UNIT_SIZE)
+    # print(x, y)
+    outline_rect = pygame.Rect(x, y, WIDTH, HEIGHT)
+    pygame.draw.rect(screen, COLOR_WHITE, outline_rect, 2)
 
 # window = tk.Tk()
 # window.mainloop()
@@ -82,11 +112,19 @@ def main():
     background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
     background.fill(pygame.Color('#456000'))
 
+    cursor_img = pygame.image.load('resource/cursor/RPG Style Arrow Alt.cur')
+    horizontal_cursor = pygame.image.load('resource/cursor/RPG Style Horizontal Resize.cur')
+    vertical_cursor = pygame.image.load('resource/cursor/RPG Style Vertical Resize.cur')
+    pygame.mouse.set_visible(False)
+    cursor_img_rect = cursor_img.get_rect()
+
     running = True
 
     while running:
-        # fpsClock.tick(30)
+        fpsClock.tick(60)
 
+        cursor_img_rect.center = pygame.mouse.get_pos()  # update position 
+        
         # print(pygame.time.get_ticks())
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -96,16 +134,24 @@ def main():
                 mouse_pos = pygame.mouse.get_pos()
                 print(mouse_pos)
 
-                hwnd = pygame.display.get_wm_info()["window"]
-                prototype = WINFUNCTYPE(BOOL, HWND, POINTER(RECT))
-                paramflags = (1, "hwnd"), (2, "lprect")
-                GetWindowRect = prototype(("GetWindowRect", windll.user32), paramflags)
-                # finally get our data!
-                rect = GetWindowRect(hwnd)
-                # print(rect.top, rect.left, rect.bottom, rect.right)
-                
-                inputDialog = MyDialog(root, (rect.left, rect.top), mouse_pos)
-                root.wait_window(inputDialog.top)
+                for item in all_sprites.sprites():
+                    if item.rect.collidepoint(mouse_pos):
+                        hwnd = pygame.display.get_wm_info()["window"]
+                        prototype = WINFUNCTYPE(BOOL, HWND, POINTER(RECT))
+                        paramflags = (1, "hwnd"), (2, "lprect")
+                        GetWindowRect = prototype(("GetWindowRect", windll.user32), paramflags)
+                        # finally get our data!
+                        rect = GetWindowRect(hwnd)
+                        # print(rect.top, rect.left, rect.bottom, rect.right)
+
+                        # cursor_img_rect.center = (1000, 1000)
+                        # screen.blit(cursor_img, cursor_img_rect) # draw the cursor
+                        # # screen.fill((255, 0, 0))
+                        # pygame.display.update()
+                        
+                        inputDialog = MyDialog(root, (rect.left, rect.top), mouse_pos)
+                        root.wait_window(inputDialog.top)
+                        break
 
         
         # inputDialog = MyDialog(root)
@@ -120,6 +166,16 @@ def main():
         # screen.fill((0, 0, 0))
         screen.blit(background, (0, 0))
         all_sprites.draw(screen)
+        
+        mouse_pos = pygame.mouse.get_pos()
+        if mouse_pos[0] >= SCREEN_WIDTH - UNIT_SIZE / 2 or mouse_pos[0] < UNIT_SIZE / 2:
+            screen.blit(horizontal_cursor, cursor_img_rect)
+        elif mouse_pos[1] >= SCREEN_HEIGHT - UNIT_SIZE / 2 or mouse_pos[1] < UNIT_SIZE / 2:
+            screen.blit(vertical_cursor, cursor_img_rect)
+        else:
+            screen.blit(cursor_img, cursor_img_rect)
+
+        draw_mousepos(screen, mouse_pos)
         pygame.display.update()
 
 if __name__ == "__main__":
